69688ff12be10933a51be0f85e6edcd6804d28ca
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-07-31 15:35:13 +0200

    Implement util methods to detect HDF5 signature

---
+++ src/main/java/org/magicdgs/hdf5j/HDF5Utils.java
@@ -0,0 +1,76 @@
+package org.magicdgs.hdf5j;
+
+import com.google.common.base.Preconditions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * Utilities for HDF5 files.
+ *
+ * @author Daniel Gomez-Sanchez (magicDGS)
+ */
+public final class HDF5Utils {
+
+    // cannot be instantiated
+    private HDF5Utils() {}
+
+    /**
+     * Check if the beginning of an {@link InputStream} is in the HDF5 format; that is, starts with
+     * the {@link HDF5Constants#FORMAT_SIGNATURE}.
+     *
+     * @param inputStream input stream with mark support.
+     *
+     * @return {@code true} if input stream is an HDF5 formatted; {@code false} otherwise.
+     *
+     * @throws NullPointerException     if the input stream is {@code null}.
+     * @throws IllegalArgumentException if mark is not supported for the input stream.
+     * @throws IOException              if an IO error occurs.
+     */
+    public static boolean isHDF5Stream(final InputStream inputStream) throws IOException {
+        Preconditions.checkNotNull(inputStream, "null inputStream");
+        Preconditions.checkArgument(inputStream.markSupported(),
+                "cannot check an inputStream without mark support: %s", inputStream);
+        // mark the stream to come back
+        inputStream.mark(HDF5Constants.FORMAT_SIGNATURE.length);
+        boolean isHDF5 = readHDF5signature(inputStream);
+        // reset the stream
+        inputStream.reset();
+        return isHDF5;
+    }
+
+    /**
+     * Check if the start of the file is an HDF5 formatted file; that is, it starts with the
+     * {@link HDF5Constants#FORMAT_SIGNATURE}.
+     *
+     * @param path file to check.
+     *
+     * @return {@code true} if the file is HDF5 formatted; {@code false} otherwise.
+     *
+     * @throws IOException if an IO error occurs.
+     */
+    public static boolean isHDF5File(final Path path) throws IOException {
+        Preconditions.checkNotNull(path, "null path");
+        // auto-closing with try-with-resources
+        try (final InputStream is = Files.newInputStream(path, StandardOpenOption.READ)) {
+            // does not require mark
+            return readHDF5signature(is);
+        }
+    }
+
+    // helper method which consume the InputStream for avoid the usage of mark if it isn't necessary
+    private static boolean readHDF5signature(final InputStream inputStream) throws IOException {
+        // get the possible HDF5 signature
+        for (int i = 0; i < HDF5Constants.FORMAT_SIGNATURE.length; i++) {
+            final int currentByte = inputStream.read();
+            if (currentByte == -1 || currentByte != HDF5Constants.FORMAT_SIGNATURE[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/org/magicdgs/hdf5j/HDF5UtilsTest.java
@@ -0,0 +1,106 @@
+package org.magicdgs.hdf5j;
+
+import com.google.common.primitives.Bytes;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.internal.collections.Ints;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Daniel Gomez-Sanchez (magicDGS)
+ */
+public class HDF5UtilsTest extends HDF5jTest {
+
+    // temp directory for creating files
+    private Path tmpDir;
+
+    @BeforeClass
+    public void setUp() throws IOException {
+        tmpDir = Files.createTempDirectory(this.getClass().getName());
+    }
+
+    @DataProvider
+    public Object[][] invalidStreams() {
+        return new Object[][] {
+                // null stream should throw
+                {null, NullPointerException.class},
+                // also stream without mark support (default for InputStream abstract class)
+                {new InputStream() {
+                    @Override
+                    public int read() throws IOException {
+                        return 0;
+                    }
+                }, IllegalArgumentException.class}
+        };
+    }
+
+    @Test(dataProvider = "invalidStreams")
+    public void testIsHDF5StreamInvalid(final InputStream invalidStream,
+            final Class<? extends Throwable> e) throws Exception {
+        Assert.assertThrows(e, () -> HDF5Utils.isHDF5Stream(invalidStream));
+    }
+
+    @Test
+    public void testIsHDF5Stream() throws Exception {
+        // HDF5 stream
+        final InputStream hdf5SignatureStream = new ByteArrayInputStream(
+                Bytes.toArray(Ints.asList(HDF5Constants.FORMAT_SIGNATURE)));
+        // check that a stream with the HDF5 signature returns true
+        Assert.assertTrue(HDF5Utils.isHDF5Stream(hdf5SignatureStream));
+        // and because it is not consumed, it could be passed again
+        Assert.assertTrue(HDF5Utils.isHDF5Stream(hdf5SignatureStream));
+
+        // Text stream
+        final InputStream textStream = new ByteArrayInputStream("ABCD".getBytes());
+        // check that some text is not HDF5 stream
+        Assert.assertFalse(HDF5Utils.isHDF5Stream(textStream));
+        // check that still could be read
+        Assert.assertEquals(textStream.read(), 'A');
+    }
+
+    @DataProvider
+    public Iterator<Object[]> testFiles() throws IOException {
+        final List<Object[]> data = new ArrayList<>();
+        // add all the examples by API, that are real HDF5
+        HDF5jTestResources.getExamplesByApi().forEach(p -> data.add(new Object[] {p, true}));
+        // add empty file to the dataset (no HDF5)
+        data.add(new Object[] {Files.createFile(tmpDir.resolve("empty.h5")), false});
+        // text file with more than 8 bytes (and CR-LF spacing)
+        data.add(new Object[] {
+                createTempFileWithContent("text.h5", "HDF5\r\nThis is a text file\r\n"), false});
+        // corrupted HDF5 file (no end of line)
+        data.add(
+                new Object[] {createTempFileWithContent("corrupted.h5", "\211HDF\r\n\032Corrupted"),
+                        false});
+        // short file (less than 8 bytes, but first bytes the same as HDF5 signature
+        data.add(new Object[] {createTempFileWithContent("7bytes.h5", "\211HDF\r\n\032"), false});
+        return data.iterator();
+
+    }
+
+    private Path createTempFileWithContent(final String name, final String content)
+            throws IOException {
+        final Path path = Files.createFile(tmpDir.resolve(name));
+        try (final Writer os = Files.newBufferedWriter(path)) {
+            os.write(content);
+        }
+        return path;
+    }
+
+    @Test(dataProvider = "testFiles")
+    public void testIsHDF5File(final Path path, final boolean isHDF5) throws Exception {
+        Assert.assertEquals(HDF5Utils.isHDF5File(path), isHDF5);
+    }
+
+}
